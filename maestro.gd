1bf2f9b9d0507db37fbeae619dcbf50c4a04b9b1
hiroshi <eduhito57@gmail.com>
2024-11-05 23:41:32 -0300

    added animations based on direction movement

--- maestro.gd
+++ maestro.gd
@@ -1,19 +1,42 @@
 extends CharacterBody2D
 
 @export var speed = 400
+var last_direction = Vector2.ZERO
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("left", "right", "up", "down")	
 	velocity = input_direction * speed
-	$AnimatedSprite2D.play()
 	
-	if velocity.x != 0 or velocity.y != 0:
-		$AnimatedSprite2D.animation = "idle"
-		$AnimatedSprite2D.flip_v = false
+	if input_direction != Vector2.ZERO:
+		last_direction = input_direction
+	
+	# horizontal movement
+	if velocity.x != 0 and velocity.y == 0:
+		$AnimatedSprite2D.play("side")
 		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	# diagonal movement
+	elif velocity.x != 0 and velocity.y < 0:
+		$AnimatedSprite2D.play("up_diagonal")
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.x != 0 and velocity.y > 0:
+		$AnimatedSprite2D.play("down_diagonal")
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	
+	#vertical movement
+	elif velocity.x == 0 and velocity.y < 0:
+		$AnimatedSprite2D.play("up")
+	elif velocity.x == 0 and velocity.y > 0:
+		$AnimatedSprite2D.play("down")
+	
+	# idle
 	else:
-		$AnimatedSprite2D.animation = "idle"
-
+		if last_direction.y != -1:
+			$AnimatedSprite2D.play("idle_down")
+		else:
+			$AnimatedSprite2D.play("idle_up")
+		$AnimatedSprite2D.flip_h = true if last_direction.x != 1 else false 
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
