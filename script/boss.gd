c4bdd21af0d21a25bf2dadf99b6b0a5f56846bd0
hiroshi <eduhito57@gmail.com>
2024-11-11 00:12:23 -0300

    added attack logic

--- script/boss.gd
+++ script/boss.gd
@@ -3,14 +3,19 @@ extends CharacterBody2D
 var speed = 200
 var health = 100
 var damage
+var can_be_damaged = true
 var dead = false
 var player_in_area = false
+var player_in_range = false
 var player
 
 func _ready():
 	dead = false
 
 func _physics_process(delta):
+	deal_with_damage()
+	
+	
 	if !dead:
 		$detection_area/CollisionShape2D.disabled = false
 		if player_in_area:
@@ -21,6 +26,9 @@ func _physics_process(delta):
 	else:
 		$detection_area/CollisionShape2D.disabled = true
 
+func boss():
+	pass
+
 func _on_detection_area_body_entered(body):
 	if body.has_method("player"):
 		player_in_area = true
@@ -30,3 +38,25 @@ func _on_detection_area_body_exited(body):
 	if body.has_method("player"):
 		player_in_area = false
 		player = body
+
+func _on_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_in_range = true
+
+func _on_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_in_range = false
+
+func deal_with_damage():
+	if player_in_range and global.player_current_attack == true:
+		if can_be_damaged == true:
+			$takeDamage.start()
+			can_be_damaged = false
+			health = health - 10
+			print("boss - 10")
+			if health <= 0:
+				self.queue_free()
+
+
+func _on_take_damage_timeout() -> void:
+	can_be_damaged = true
--- script/player.gd
+++ script/player.gd
@@ -2,6 +2,11 @@ extends CharacterBody2D
 
 @export var speed = 500
 var last_direction = Vector2.ZERO
+var enemy_in_range = false
+var enemy_attack_cooldown = true
+var health = 100
+var is_alive = true
+var is_attacking = false
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")	
@@ -43,3 +48,47 @@ func player():
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+	enemy_attack()
+	attack()
+	
+	if health <= 0:
+		is_alive = false #add menu later
+		health = 0
+		print("you died")
+	
+
+func enemy_attack():
+	if enemy_in_range and enemy_attack_cooldown:
+		health = health - 1
+		enemy_attack_cooldown = false
+		$attackCooldown.start()
+		print(health)
+
+func attack():
+	var dir = last_direction
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		is_attacking = true
+		
+		##right
+		#if dir.x == 1 and dir.y == 0:
+		
+		$attackAnim.play("attackA")
+		$dealAttackTimer.start()
+			
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("boss"):
+		enemy_in_range = true
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("boss"):
+		enemy_in_range = false
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+func _on_deal_attack_timer_timeout() -> void:
+	$dealAttackTimer.stop()
+	global.player_current_attack = false
